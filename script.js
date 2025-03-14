a39ad734378fa72c8e58add8ea91a83f77c943e6
felipedoprado <145500571+felipedoprado@users.noreply.github.com>
2025-03-14 19:13:30 +0000

    rotação 2

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', function(){
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
-    
-    botaoDeAcessibilidade.addEventListener('click', function (){
-        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
-    });
 
+    botaoDeAcessibilidade.addEventListener('click', function (){
+       botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+       opcoesDeAcessibilidade.classList.toggle('apresenta-lista');  
+});
+    
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
@@ -18,5 +18,6 @@ document.addEventListener('DOMContentLoaded', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
+
 
 });

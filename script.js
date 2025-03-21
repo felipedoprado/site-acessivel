dee8536115888dc480ca9cde0402916bb289faaa
felipedoprado <145500571+felipedoprado@users.noreply.github.com>
2025-03-21 17:11:52 +0000

    script correção

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
-    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
-    botaoDeAcessibilidade.addEventListener('click', function (){
-       botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-       opcoesDeAcessibilidade.classList.toggle('apresenta-lista');  
-});
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+    })
     
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
